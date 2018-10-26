2fe6166ba55cf1dc43da3c7344d6924582b2304c
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-10-26 15:09:20 -0700

    mm/zsmalloc.c: fix fall-through annotation
    Replace "fallthru" with a proper "fall through" annotation.
    
    This fix is part of the ongoing efforts to enabling
    -Wimplicit-fallthrough
    
    Link: http://lkml.kernel.org/r/20181003105114.GA24423@embeddedor.com
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -435,7 +435,7 @@ static void *zs_zpool_map(void *pool, unsigned long handle,
 	case ZPOOL_MM_WO:
 		zs_mm = ZS_MM_WO;
 		break;
-	case ZPOOL_MM_RW: /* fallthru */
+	case ZPOOL_MM_RW: /* fall through */
 	default:
 		zs_mm = ZS_MM_RW;
 		break;
@@ -1346,7 +1346,7 @@ static bool can_merge(struct size_class *prev, int pages_per_zspage,
 		return false;
 
 	if (get_maxobj_per_zspage(prev->size, prev->pages_per_zspage)
-		!= get_maxobj_per_zspage(size, pages_per_zspage))
+		!= get_maxobj_per_zspage(ksize, pages_per_zspage))
 		return false;
 
 	return true;
