b30f103f833fd28f0d1f31c0afd19342528060f3
Gian <gianpaoloestacio5@gmail.com>
2022-07-24 07:23:04 +0000

    kernel: Fix my Mistakes extended commit: 4a226117eccf10bfc31d75c32ce1a308634da896 * it silence also warning
    Signed-off-by: Gian <gianpaoloestacio5@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1234,16 +1234,16 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 
 	/* Force all performance-critical kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
-		new_mask = cpu_cpu_mask;
+		new_mask = cpu_perf_mask;
 	
 	/* Force all low-power kthreads onto the little cluster */
 	if (p->flags & PF_LOW_POWER)
-		new_mask = cpu_cpu_mask;
+		new_mask = cpu_lp_mask;
 	
 	/* Force all trivial, unbound kthreads onto the big cluster */
 	if (p->flags & PF_KTHREAD && p->pid != 1 &&
 		cpumask_equal(new_mask, cpu_all_mask))
-		new_mask = cpu_cpu_mask;
+		new_mask = cpu_perf_mask;
 
 	rq = task_rq_lock(p, &flags);
 
