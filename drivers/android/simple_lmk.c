STRIX-Project/STRIX_kernel_xiaomi-sdm660

simple_lmk: Boost cpus and DDR to maxfreq when lmk decide to kill a task
* Lags always happen here

Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

drivers/android/simple_lmk.c
@@ -11,6 +11,8 @@
 #include <linux/oom.h>
 #include <linux/sort.h>
 #include <linux/vmpressure.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 /* The minimum number of pages to free per reclaim */
 #define MIN_FREE_PAGES (CONFIG_ANDROID_SIMPLE_LMK_MINFREE * SZ_1M / PAGE_SIZE)
@@ -231,6 +233,9 @@ static void scan_and_kill(void)
 		pr_info("Killing %s with adj %d to free %lu KiB\n", vtsk->comm,
 			vtsk->signal->oom_score_adj,
 			victim->size << (PAGE_SHIFT - 10));
+
+		cpu_input_boost_kick_max(500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW,500);
 
 		/* Accelerate the victim's death by forcing the kill signal */
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);

parikk/kernel_samsung_universal7904

drivers/android/simple_lmk.c
@@ -12,6 +12,8 @@
 #include <linux/oom.h>
 #include <linux/sort.h>
 #include <linux/vmpressure.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 /* Needed to prevent Android from thinking there's no LMK and thus rebooting */
 #undef MODULE_PARAM_PREFIX
@@ -241,11 +243,8 @@ static void scan_and_kill(void)
 			vtsk->signal->oom_score_adj,
 			victim->size << (PAGE_SHIFT - 10));
 
-		/* Count kills */
-		adj_index = vtsk->signal->oom_score_adj / ADJ_DIVISOR;
-		if (adj_index > (ADJ_MAX / ADJ_DIVISOR))
-			adj_index = (ADJ_MAX / ADJ_DIVISOR);
-		lmk_count[adj_index]++;
+		cpu_input_boost_kick_max(500);
+		devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF,500);
 
 		/* Accelerate the victim's death by forcing the kill signal */
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);